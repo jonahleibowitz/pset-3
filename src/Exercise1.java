8418d8a2d9ad477c8a884d0e6fa7eaf37bb79d40
Jonah Leibowitz <jonahleibowitz@comcast.net>
2020-09-21 10:15:16 -0400

    completed exercise 1

---
+++ src/Exercise1.java
@@ -0,0 +1,7 @@
+public class Exercise1 {
+  public static void main(String[] args) {
+      System.out.println("H E L L O" + System.lineSeparator() + "E       A"+
+              System.lineSeparator() + "L       P" + System.lineSeparator() + "L       C" +
+              System.lineSeparator() + "O A P C S");
+  }
+}
