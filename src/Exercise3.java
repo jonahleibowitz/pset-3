679dfb78d455c8eab509673c498c7f20c0a17a9b
Jonah Leibowitz <jonahleibowitz@comcast.net>
2020-09-21 13:10:06 -0400

    fixed decimal rounding for ex3

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class Exercise3 {
+    private static DecimalFormat df = new DecimalFormat("0.00");
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -10,7 +13,8 @@ public class Exercise3 {
         int width = in.nextInt();
         System.out.println("Height:" + height + '\n' + "Width:" + width);
             double area= (height * 25.4) * (width * 25.4) ; //May not be exact conversion rate between inches and mm but should be close enough
-                    System.out.println('\n' + area + " square millimeters");
+        df.setRoundingMode(RoundingMode.UP);
+                    System.out.println('\n' + df.format(area) + " square millimeters");
 
         in.close();
     }
