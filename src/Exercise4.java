793bead339b347a73bb04f2e55b96c85b9272ce8
Jonah Leibowitz <jonahleibowitz@comcast.net>
2020-09-21 20:04:14 -0400

    finished ex4

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -3,20 +3,20 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class Exercise4 {
-    private static DecimalFormat df = new DecimalFormat("0.00");
+    private static DecimalFormat formatter = new DecimalFormat("#,###.00");
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Height:");
-        int height = in.nextInt();
+        double height = in.nextDouble();
         System.out.println("Width:");
-        int width = in.nextInt();
-        System.out.println("Height:" + height + '\n' + "Width:" + width);
-        double perimeter= (height * 2.54) * (width * 2.54) ;
-        df.setRoundingMode(RoundingMode.UP);
-        System.out.println('\n' + df.format(perimeter) + " centimeters");
+        double width = in.nextDouble();
+        System.out.println("Height : " + height + '\n' + "Width  : " + width);
+        double perimeter= ((height * 2.54)*2) + ((width * 2.54)*2) ;
+
+        System.out.println('\n' + formatter.format(perimeter) + " centimeters");
+
 
         in.close();
     }
 }
-
